a2de36499677299a7ad9d1ca6e12e0f0abfb61ea
Tushar Gupta <tusharguptagps@gmail.com>
2024-09-28 18:30:58 +0530

    Update squareCharPattern.js

--- Basic_Module/Questions/squareCharPattern.js
+++ Basic_Module/Questions/squareCharPattern.js
@@ -1,7 +1,7 @@
 function patternPrint(n){
-    for(let i=0; i < n; i++){
+    for(let i=0; i < n; i++){ // outer Column
         let row = " "
-        for(let j=0; j < n; j++){
+        for(let j=0; j < n; j++){ // inner row
             row += String.fromCharCode(65+j)
         }
         console.log(row)
