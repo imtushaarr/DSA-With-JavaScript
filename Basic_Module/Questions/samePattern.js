6fc9671328c876030ae23f491755d32ad12bd66b
Tushar Gupta <tusharguptagps@gmail.com>
2024-09-28 19:41:52 +0530

    Create samePattern.js

---
+++ Basic_Module/Questions/samePattern.js
@@ -0,0 +1,12 @@
+function samePattern(n){
+    for(let i=1; i<n ; i++){
+        let row = " "
+        for(let j=1; j<=i; j++){
+            row += i
+        }
+        console.log(row)
+    }
+}
+
+let n = 4;
+samePattern(n)
